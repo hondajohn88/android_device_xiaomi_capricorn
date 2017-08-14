cd1a3cf4ea37db823c0eeb5a9b7a8df9a69223a2
hondajohn88 <hondajohn88@gmail.com>
2017-08-13 20:32:37 -0700

    Initial bringup for XenonHD

--- xenonhd.mk
+++ xenonhd.mk
@@ -1,14 +1,17 @@
 $(call inherit-product, device/xiaomi/scorpio/full_scorpio.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
-PRODUCT_NAME := tesla_scorpio
+PRODUCT_NAME := xenonhd_scorpio
 PRODUCT_DEVICE := scorpio
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI Note 2
 PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+# Root options
+ROOT_METHOD=magisk
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -17,5 +20,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="scorpio" \
     BUILD_FINGERPRINT="Xiaomi/scorpio/scorpio:6.0.1/MXB48T/V8.2.6.0.MADCNDL:user/release-keys" \
     PRIVATE_BUILD_DESC="scorpio-user 6.0.1 MXB48T V8.2.6.0.MADCNDL release-keys"
-
-TARGET_VENDOR := xiaomi
